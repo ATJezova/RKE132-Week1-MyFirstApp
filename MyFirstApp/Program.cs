cd487d3fe453ea6621631964f47f7a84c0607043
ATJezova <anna-triin.jezova@tktk.ee>
2023-01-27 18:10:38 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //intput
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
